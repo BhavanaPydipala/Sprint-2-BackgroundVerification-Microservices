e9ace6ec121550c251928e3f32551225b166e860
pydipalabhavana07@gmail.com <pydipalabhavana07@gmail.com>
2020-04-24 20:10:08 +0530

    commit

---
+++ FetchEmployeeDocumentByName-service/src/main/java/com/cg/backgroundverification/FetchEmployeeDocumentByNameServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cg.backgroundverification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FetchEmployeeDocumentByNameServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FetchEmployeeDocumentByNameServiceApplication.class, args);
+	}
+
+}
